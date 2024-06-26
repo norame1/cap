65d57fb969da11674d325eb67bfb80858c31b40c
Gautam Santhosh <gautamsanthosh123@gmail.com>
2024-06-26 18:14:05 +0530

    first

---
+++ Project/Assets/ML-Agents/Examples/Pyramids/Scripts/PyramidAgent.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class PyramidAgent : Agent
+{
+    public GameObject area;
+    private PyramidArea m_MyArea;
+    private Rigidbody m_AgentRb;
+    private PyramidSwitch m_SwitchLogic;
+    public GameObject areaSwitch;
+    public bool useVectorObs;
+
+    public override void Initialize()
+    {
+        m_AgentRb = GetComponent<Rigidbody>();
+        m_MyArea = area.GetComponent<PyramidArea>();
+        m_SwitchLogic = areaSwitch.GetComponent<PyramidSwitch>();
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        if (useVectorObs)
+        {
+            sensor.AddObservation(m_SwitchLogic.GetState());
+            sensor.AddObservation(transform.InverseTransformDirection(m_AgentRb.velocity));
+        }
+    }
+
+    public void MoveAgent(ActionSegment<int> act)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var action = act[0];
+        switch (action)
+        {
+            case 1:
+                dirToGo = transform.forward * 1f;
+                break;
+            case 2:
+                dirToGo = transform.forward * -1f;
+                break;
+            case 3:
+                rotateDir = transform.up * 1f;
+                break;
+            case 4:
+                rotateDir = transform.up * -1f;
+                break;
+        }
+        transform.Rotate(rotateDir, Time.deltaTime * 200f);
+        m_AgentRb.AddForce(dirToGo * 2f, ForceMode.VelocityChange);
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        AddReward(-1f / MaxStep);
+        MoveAgent(actionBuffers.DiscreteActions);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[0] = 3;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[0] = 4;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        RespawnAgentAndSwitch();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("switchOn"))
+        {
+            SetReward(2f);
+            EndEpisode();
+        }
+    }
+
+    public void OnSwitchActivated()
+    {
+        SetReward(1f);
+        RespawnAgentAndSwitch();
+    }
+
+    private void RespawnAgentAndSwitch()
+    {
+        var spawnIndexes = Enumerable.Range(0, m_MyArea.spawnAreas.Length).OrderBy(x => Guid.NewGuid()).Take(2).ToArray();
+
+        m_AgentRb.velocity = Vector3.zero;
+        m_AgentRb.angularVelocity = Vector3.zero;
+
+        m_MyArea.PlaceObject(gameObject, spawnIndexes[0]);
+        transform.rotation = Quaternion.Euler(0f, Random.Range(0, 360f), 0f);
+
+        m_SwitchLogic.ResetSwitch(spawnIndexes[1]);
+    }
+}
